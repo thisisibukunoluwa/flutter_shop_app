7538430cfd35cbd227b224efbb741d9a9111c4fc
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-07-28 19:12:08 +0100

    feat:added widgets folder

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shop_app/screens/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (ctx) => ProductDetailScreen(title: title,)));
+              },
+              child: Image.network(
+                imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            footer: GridTileBar(
+                title: Text(
+                  title,
+                  textAlign: TextAlign.center,
+                ),
+                backgroundColor: Colors.black87,
+                leading: IconButton(
+                    icon: Icon(Icons.favorite),
+                    onPressed: () {},
+                    color: Theme.of(context).colorScheme.secondary),
+                trailing: IconButton(
+                  icon: Icon(Icons.shopping_cart),
+                  onPressed: () {},
+                  color: Theme.of(context).colorScheme.secondary,
+                ))));
+  }
+}
