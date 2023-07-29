5b66015d1e4bcc15f393a18ddd7c8ddc4796e0b3
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-07-29 14:46:42 +0100

    feat:added various providers and their models

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Product with ChangeNotifier {
+
   final String id;
   final String title;
   final String description;
@@ -11,10 +12,11 @@ class Product with ChangeNotifier {
   Product(
       {required this.id,
       required this.description,
-      required this.title,
       required this.price,
+      required this.title,
       required this.imageUrl,
       this.isFavorite = false});
+
 
   void toggleFavoriteStatus() {
     isFavorite = !isFavorite;
