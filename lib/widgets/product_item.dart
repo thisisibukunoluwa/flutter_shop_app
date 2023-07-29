220e8614e62076bf35c2467eb219c442ef30f065
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-07-29 14:52:36 +0100

    update:added product_item widget as GridTile wrapped it in ClipRRect

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,42 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop_app/providers/product.dart';
 import 'package:flutter_shop_app/screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
+  // final String id;
+  // final String title;
+  // final String imageUrl;
 
-  ProductItem(this.id, this.title, this.imageUrl);
+  // ProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
-        borderRadius: BorderRadius.circular(10),
-        child: GridTile(
-            child: GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (ctx) => ProductDetailScreen(title: title,)));
-              },
-              child: Image.network(
-                imageUrl,
-                fit: BoxFit.cover,
-              ),
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                  arguments: product.id);
+            },
+            child: Image.network(
+              product.imageUrl,
+              fit: BoxFit.cover,
             ),
-            footer: GridTileBar(
-                title: Text(
-                  title,
-                  textAlign: TextAlign.center,
-                ),
-                backgroundColor: Colors.black87,
-                leading: IconButton(
-                    icon: Icon(Icons.favorite),
-                    onPressed: () {},
+          ),
+          footer: GridTileBar(
+              title: Text(
+                product.title,
+                textAlign: TextAlign.center,
+              ),
+              backgroundColor: Colors.black87,
+              // we shrink the area of the widget that reruns when our product changes
+              // so we just make it our leading icon
+              leading: Consumer<Product>(
+                builder: (ctx, product, child) => IconButton(
+                    icon: Icon(product.isFavorite
+                        ? Icons.favorite
+                        : Icons.favorite_border),
+                    onPressed: () {
+                      product.toggleFavoriteStatus();
+                    },
                     color: Theme.of(context).colorScheme.secondary),
-                trailing: IconButton(
-                  icon: Icon(Icons.shopping_cart),
-                  onPressed: () {},
-                  color: Theme.of(context).colorScheme.secondary,
-                ))));
+              ),
+              trailing: IconButton(
+                icon: Icon(Icons.shopping_cart),
+                onPressed: () {},
+                color: Theme.of(context).colorScheme.secondary,
+              ))),
+    );
   }
 }
