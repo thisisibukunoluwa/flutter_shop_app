4e8955e89c4e4c7cc69a06dddddca175bc3836e4
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-07-29 11:10:32 +0100

    feat created prducts_grid as a reusable widget

---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shop_app/providers/products.dart';
+import 'package:provider/provider.dart';
+import '../providers/product.dart';
+import 'product_item.dart';
+
+class ProductsGrid extends StatelessWidget {
+  final bool showFavs;
+
+  ProductsGrid(this.showFavs);
+
+  @override
+  Widget build(BuildContext context) {
+    //will rebuild when the parent of the object widget changes
+    // we are establishing a direct communication channel with the provided instance of the provider class
+
+    final productsData = Provider.of<Products>(context);
+
+    final products = showFavs ? productsData.favoriteItems : productsData.items;
+
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      itemBuilder: (ctx, i) => ChangeNotifierProvider<Product>.value(
+        value: products[i],
+        child: ProductItem(
+            // products[i].id,
+            // products[i].title,
+            // products[i].imageUrl,
+            ),
+      ),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10),
+    );
+  }
+}
